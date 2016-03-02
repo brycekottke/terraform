71f37fa1144b53d3151cab8c3bf5ac545654e782
Bryce Kottke <bkottke@datapipe.com>
2016-03-02 10:19:47 -0800

    developed terraform configs

---
+++ configs/ec2/aws-ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "${var.region}"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-9abea4fb"
+    instance_type = "t2.micro"
+    tags {
+        Name = "dev-terraform-1"
+        environment = "dev"
+    }
+}
